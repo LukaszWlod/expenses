260ce3c5256dbe0febcbe58ae5b55e21147eb9a6
LukaszWlod <lukasz1023w@gmail.com>
2020-09-25 11:07:59 +0200

    Add method cosPerMonth to class fueal

--- src/com/lukasz/Fuel.java
+++ src/com/lukasz/Fuel.java
@@ -1,28 +1,40 @@
 package com.lukasz;
 
 public class Fuel {
- private  double averageFuelEconomyPer100km;
- private double costPerLitre;
- private double distanceToWork;
- private double costPerDay;
+    private double averageFuelEconomyPer100km;
+    private double costPerLitre;
+    private double distanceToWork;
+    private double costWorkDay;
+    private short workingDays;
 
- public Fuel(){}
- public Fuel (
-         double averageFuelEconomyPer100km,
-         double costPerLitre,
-         double distanceToWork
- ){
-   this.averageFuelEconomyPer100km=averageFuelEconomyPer100km;
-   this.costPerLitre=costPerLitre;
-   this.distanceToWork=distanceToWork;
 
-     double costPerKm =averageFuelEconomyPer100km/100*costPerLitre;
-     costPerDay=costPerKm*distanceToWork;
- }
+    public Fuel() {
+    }
 
- public  double getCostPerDay(){
-     return costPerDay;
- }
+    public Fuel(
+            double averageFuelEconomyPer100km,
+            double costPerLitre,
+            double distanceToWork,
+            short workingDays
+    ) {
+        this.averageFuelEconomyPer100km = averageFuelEconomyPer100km;
+        this.costPerLitre = costPerLitre;
+        this.distanceToWork = distanceToWork;
+        this.workingDays=workingDays;
+
+        double costPerKm = averageFuelEconomyPer100km / 100 * costPerLitre;
+        costWorkDay = costPerKm * distanceToWork;
+    }
+
+    public double getCostPerDay() {
+        return costWorkDay;
+    }
+
+    public double getCostPerMouth() {
+        double costPerMount = getCostPerDay() * workingDays;
+        return costPerMount;
+    }
+
 
 
 
